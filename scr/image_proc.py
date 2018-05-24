031c3b74f5bb3206986761efdc3cb64f22c51726
HugoBoss1212 <lechowicz2@gmail.com>
2018-05-24 23:12:41 +0200

    komit na koniec dnia

--- scr/image_proc.py
+++ scr/image_proc.py
@@ -2,14 +2,16 @@ import matplotlib.pyplot as plt
 from skimage.filters import sobel
 from skimage import exposure
 from skimage.util import invert
-from skimage.morphology import binary_closing, watershed
-from skimage.feature import peak_local_max
+from skimage.morphology import binary_closing, watershed, binary_erosion, binary_dilation, opening, erosion, square, dilation
+from skimage.feature import peak_local_max, canny
+from skimage.transform import hough_circle, hough_circle_peaks
+from skimage.draw import circle_perimeter
 from scipy import ndimage as ndi
 import cv2
 import numpy as np
 
 
-def calc(image):
+def calc(image, real_num):
 
     img = cv2.threshold(image, 0.50, 255, cv2.THRESH_BINARY)[1].astype(bool)
     img = ndi.binary_closing(img)
@@ -17,11 +19,33 @@ def calc(image):
     img = binary_closing(img)
     img = ndi.binary_fill_holes(img)
 
-    distance = ndi.distance_transform_edt(img)
-    local_maxi = peak_local_max(distance, indices=False, footprint=np.ones((3, 3)),
-                                labels=img)
+    img = binary_dilation(img)
+    img = binary_erosion(img)
+
+    label_objects, nb_labels = ndi.label(img)
+    sizes = np.bincount(label_objects.ravel())
+
+    temp = 0
+    while nb_labels != real_num:
+        label_objects = binary_erosion(label_objects)
+        label_objects, nb_labels = ndi.label(label_objects)
+        temp += 1
+    print(str(temp) + " number of erosions ")
+    for i in range(0, temp):
+        label_objects = binary_dilation(label_objects)
+
+    label_objects, nb_labels = ndi.label(label_objects)
+    sizes = np.bincount(label_objects.ravel())
+
+    distance = ndi.distance_transform_edt(label_objects)
+    local_maxi = peak_local_max(distance, indices=False, footprint=np.ones((3, 3)), min_distance=20,
+                                labels=label_objects)
     markers = ndi.label(local_maxi)[0]
-    img = watershed(- distance, markers, mask=img)
+    img = watershed(- distance, markers, mask=label_objects)
+
+    print(str(nb_labels) + " number of objects")
+    # for i in sizes:
+    #     print(str(i) + " wielkosci obiektow")
 
     plt.imshow(img, cmap="hot")
     plt.axis("off")
--- scr/main.py
+++ scr/main.py
@@ -6,9 +6,10 @@ import glob
 
 def main_():
     for file in glob.glob("images/*.jpg"):
+        count = int(file[len(file)-6:len(file)-4])
         image = io.imread(file)
         image = rgb2gray(image)
-        image_proc.calc(image)
+        image_proc.calc(image, count)
 
 
 if __name__ == '__main__':
