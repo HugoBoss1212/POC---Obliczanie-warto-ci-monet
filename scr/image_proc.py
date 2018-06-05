f25bdbf63c2c52234bb2a8bbf5216e2d5e10a8f7
Kacper <lechowicz2@gmail.com>
2018-06-05 12:38:46 +0200

    pozbywanie sie błędów nakładania wewnętrznych kół monet

--- scr/image_proc.py
+++ scr/image_proc.py
@@ -19,24 +19,24 @@ def calc(image, real_num):
     ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
     d = ndimage.distance_transform_edt(thresh)
-    local_max = peak_local_max(d, indices=False, min_distance=15,
-                               labels=thresh)
-
+    local_max = peak_local_max(d, indices=False, min_distance=15, labels=thresh)
     markers = ndimage.label(local_max, structure=np.ones((3, 3)))[0]
     labels = watershed(-d, markers, mask=thresh)
-
+    (a, b, radius) = (0, 0, 0)
     for label in np.unique(labels):
         if label == 0:
             continue
 
         mask = np.zeros(gray.shape, dtype="uint8")
         mask[labels == label] = 255
-
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                                cv2.CHAIN_APPROX_SIMPLE)[-2]
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
         c = max(cnts, key=cv2.contourArea)
-
         ((x, y), r) = cv2.minEnclosingCircle(c)
+
+        if math.sqrt(math.pow(int(x) - int(a), 2) + math.pow(int(y) - int(b), 2)) < int(r):
+            continue
+        (a, b, radius) = (x, y, r)
+
         cv2.circle(image, (int(x), int(y)), int(r), (0, 255, 0), 2)
         pole = int(math.pi*r*r)
         num = label
