50ce276479d3abf282f98d3d3984ae8f1c30a997
Kacper <lechowicz2@gmail.com>
2018-06-06 20:41:47 +0200

    computing haralick

--- scr/image_proc.py
+++ scr/image_proc.py
@@ -6,6 +6,7 @@ from scipy import ndimage
 import numpy as np
 import math
 import reference
+from scipy.spatial import distance
 
 
 def calc(image, real_num, real_value):
@@ -14,6 +15,9 @@ def calc(image, real_num, real_value):
     color_test = np.copy(image)
     monets_field = 0
     center_off_mass = []
+    feret = []
+    blair_bliss = []
+    haralick = []
 
     img = cv2.pyrMeanShiftFiltering(image, 21, 53)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -26,8 +30,10 @@ def calc(image, real_num, real_value):
     labels = watershed(-d, markers, mask=thresh)
     (a, b, radius) = (0, 0, 0)
     for label in np.unique(labels):
-        if label == 0:
-            continue
+        if label == 0: continue
+
+        # blair_bliss.append(compute_bb())
+        # feret.append(compute_f())
 
         mask = np.zeros(gray.shape, dtype="uint8")
         mask[labels == label] = 255
@@ -35,9 +41,9 @@ def calc(image, real_num, real_value):
         c = max(cnts, key=cv2.contourArea)
         ((x, y), r) = cv2.minEnclosingCircle(c)
 
-        if math.sqrt(math.pow(int(x) - int(a), 2) + math.pow(int(y) - int(b), 2)) < int(r):
-            continue
+        if math.sqrt(math.pow(int(x) - int(a), 2) + math.pow(int(y) - int(b), 2)) < int(r): continue
         center_off_mass.append((int(y), int(x)))
+        haralick.append(compute_h(r, len(c)))
         (a, b, radius) = (x, y, r)
 
         cv2.circle(image, (int(x), int(y)), int(r), (0, 255, 0), 2)
@@ -52,6 +58,11 @@ def calc(image, real_num, real_value):
         value += reference.check_value(pole, color_test[y_min:y_max, x_min:x_max])
 
     print("Środki ciężkości: " + str(center_off_mass))
+    print("Współczynniki Fereta: " + str(feret))
+    print("Współczynniki Blaira-Blissa: " + str(blair_bliss))
+    print("Współczynniki Haralicka: [ ", end="")
+    for i in haralick: print("{:.2}, ".format(i), end="")
+    print("]")
     print("Dokładność algorytmu: {:.2%}".format(check_alg(real_value, value/100, real_num, num)))
     print("Pole zajmowane przez obiekty: {:.2%}".format((monets_field*100)/(image.shape[0]*image.shape[1])/100))
     print()
@@ -72,3 +83,21 @@ def check_alg(real_value, value, real_num, num):
     if check > 2: check = 2 - (math.fabs(2 - check))
     return (check * 100)/200
 
+
+def compute_bb(points):
+    s = len(points)
+    my, mx = cog2(pts)
+    r = 0
+    for point in points:
+        r = r + distance.euclidean(point, (my, mx))**2
+    return s/(math.sqrt(2*math.pi*r))
+
+
+def compute_f():
+    pass
+
+
+def compute_h(dis, length_):
+    sum1 = dis
+    sum2 = math.pow(dis, 2)
+    return math.sqrt((math.pow(sum1, 2)) / (length_ * sum2 - 1))
